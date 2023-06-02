5d41a6c046dc9e907a2697accae918a3444c687a
Moacir David <moacirdavidag@gmail.com>
2023-06-01 21:47:56 -0300

    Corrigindo erro na url da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    url: process.env.NODE_ENV === 'local' ? 'localhost:4000' ? 'https://graphql-backend-tcc.herokuapp.com/'
+    url: process.env.NODE_ENV === 'local' ? 'localhost:4000' : 'https://graphql-backend-tcc.herokuapp.com/'
 }
 
     
