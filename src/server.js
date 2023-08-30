0e99059fd89dd447675cde20934a3a1dfdde94d6
moacirdavidag <moacirdavidag@gmail.com>
2023-08-30 13:45:30 -0300

    Configurando arquivo do servidor estático

--- src/server.js
+++ src/server.js
@@ -6,4 +6,8 @@ const app = express();
 
 app.use('/', express.static(resolve(__dirname, './build')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
 app.listen(process.env.PORT || 3000);
