7e00686ae1b36690a761d223c5ba2f42d66ade7b
moacirdavidag <moacirdavidag@gmail.com>
2023-08-30 12:40:15 -0300

    Corrigindo erro na URL da API

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    url: process.env.NODE_ENV === 'local' ? 'http://localhost:4000' : `${process.env.REACT_API_URL}`; 
+    url: process.env.NODE_ENV === 'local' ? 'http://localhost:4000' : `${process.env.REACT_API_URL}`
 }
 
     
