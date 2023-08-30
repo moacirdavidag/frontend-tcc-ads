fd30b36395b23006f139b29a842a76a6f79783e5
moacirdavidag <moacirdavidag@gmail.com>
2023-08-30 15:24:47 -0300

    Removendo uso do módulo path no servidor

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 
-const { resolve } = require('path');
-
 const app = express();
 
-app.use('/', express.static(resolve(__dirname, './public')));
+app.use(express.static('../public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile('../public/index.html');
 })
 
 app.listen(process.env.PORT || 3000);
