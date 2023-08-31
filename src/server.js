bd2c7451b85a1d1b98dd72eaabd41bb1b52ad714
moacirdavidag <moacirdavidag@gmail.com>
2023-08-31 10:14:47 -0300

    Alterando a pasta de arquivos estáticos para build no servidor

--- src/server.js
+++ src/server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, '../', 'public')));
+app.use(express.static(path.resolve(__dirname, '../', 'build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'));
 })
 
 app.listen(process.env.PORT || 3000);
